d26a6e2d0fa2011402aec6954c936c46de9c14be
Filip Troníček <filip.tronicek@seznam.cz>
2020-11-30 12:54:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ io.on('connection', socket => {
   socket.on('join-room', (roomId, userId) => {
     socket.join(roomId);
     socket.to(roomId).broadcast.emit('user-connected', userId);
+    console.log(`Peer ${userId} connected to room ${roomId}`);
 
     socket.on('disconnect', () => {
       socket.to(roomId).broadcast.emit('user-disconnected', userId);
